0fbb8da591898084160c11d278162b7c9309df67
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-17 16:54:22 +0200

    add p in main component

--- src/components/titleComponent.vue
+++ src/components/titleComponent.vue
@@ -4,6 +4,7 @@
 
 <template>
   <h1>La mia prima app con Vite!</h1>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum atque, ullam hic assumenda veniam sed laborum suscipit quos aliquam ratione reprehenderit accusamus ipsum, quam magni eius. Nulla numquam ipsum nihil.</p>
 </template>
 
 <style scoped>
