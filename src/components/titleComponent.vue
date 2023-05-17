fba1c077632731bc590f322b752285612a1a8b27
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-17 16:57:06 +0200

    add style for main component

--- src/components/titleComponent.vue
+++ src/components/titleComponent.vue
@@ -3,8 +3,10 @@
 </script>
 
 <template>
-  <h1>La mia prima app con Vite!</h1>
-  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum atque, ullam hic assumenda veniam sed laborum suscipit quos aliquam ratione reprehenderit accusamus ipsum, quam magni eius. Nulla numquam ipsum nihil.</p>
+    <div class="container">
+        <h1>La mia prima app con Vite!</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum atque, ullam hic assumenda veniam sed laborum suscipit quos aliquam ratione reprehenderit accusamus ipsum, quam magni eius. Nulla numquam ipsum nihil.</p>
+    </div>
 </template>
 
 <style scoped>
